b28e1801576e5629b37c0b5b5c2d567541878797
Ben Lechlitner <otherland@gmail.com>
2023-09-17 00:35:47 -0400

    use afero for fileio in graph command

--- cmd/graph_test.go
+++ cmd/graph_test.go
@@ -2,9 +2,12 @@ package cmd
 
 import (
 	"testing"
+	"time"
 
+	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetGraphCmd(t *testing.T) {
@@ -13,4 +16,91 @@ func TestGetGraphCmd(t *testing.T) {
 	assert.NotNil(t, got)
 	assert.IsType(t, &cobra.Command{}, got)
 	assert.Equal(t, "graph", got.Name())
+}
+
+func Test_readBenchmarkFile(t *testing.T) {
+	type args struct {
+		path    string
+		content string
+	}
+
+	tests := []struct {
+		name      string
+		args      args
+		want      *BenchmarkData
+		assertion assert.ErrorAssertionFunc
+		errText   string
+	}{
+		{
+			name: "valid benchmark file",
+			args: args{
+				path: "benchmark.json",
+				content: `{
+					"date": "2023-08-30T21:05:47Z",
+					"dir": "testdata/2015/01-testDayOne",
+					"year": "2015",
+					"day": 1,
+					"numRuns": 30,
+					"implementations": [
+					  {
+						"name": "Golang",
+						"part-one": {
+						  "mean": 0.123,
+						  "median": 0.123,
+						  "min": 0.12,
+						  "max": 0.23
+						},
+						"part-two": {
+						  "mean": 0.567,
+						  "median": 0.567,
+						  "min": 0.56,
+						  "max": 0.67
+						}
+					  }
+					]
+				  }`,
+			},
+			want: &BenchmarkData{
+				Date: time.Date(2023, 8, 30, 21, 5, 47, 0, time.UTC),
+				Dir:  "testdata/2015/01-testDayOne",
+				Year: "2015",
+				Day:  1,
+				Runs: 30,
+				Implementations: []*ImplementationData{
+					{
+						Name: "Golang",
+						PartOne: &PartData{
+							Mean:   0.123,
+							Median: 0.123,
+							Min:    0.12,
+							Max:    0.23,
+						},
+						PartTwo: &PartData{
+							Mean:   0.567,
+							Median: 0.567,
+							Min:    0.56,
+							Max:    0.67,
+						},
+					},
+				},
+			},
+			assertion: assert.NoError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fs := afero.NewMemMapFs()
+			require.NoError(t, afero.WriteFile(fs, tt.args.path, []byte(tt.args.content), 0o600))
+
+			got, err := readBenchmarkFile(fs, tt.args.path)
+
+			tt.assertion(t, err)
+			if err != nil {
+				assert.ErrorContains(t, err, tt.errText)
+			} else {
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
 }
