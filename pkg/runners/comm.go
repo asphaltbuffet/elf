b3f14ed7ec0f0272123060f30a98c54d58820131
Ben Lechlitner <otherland@gmail.com>
2023-09-18 13:57:27 -0400

    add comments for exported functions/fields

--- pkg/runners/comm.go
+++ pkg/runners/comm.go
@@ -14,17 +14,31 @@ import (
 	au "github.com/logrusorgru/aurora"
 )
 
+// A Task represents a unit of work to be performed.
 type Task struct {
-	TaskID    string `json:"task_id"`
-	Part      Part   `json:"part"`
-	Input     string `json:"input"`
+	// TaskID is the unique identifier for the task.
+	TaskID string `json:"task_id"`
+
+	// Part is the part of the work that the task should perform.
+	Part Part `json:"part"`
+
+	// Input is the input data for the task.
+	Input string `json:"input"`
+
+	// OutputDir is the directory where the task should store its output.
+	// This field is optional.
 	OutputDir string `json:"output_dir,omitempty"`
 }
 
+// A Result represents the outcome of a Task.
 type Result struct {
-	TaskID   string  `json:"task_id"`
-	Ok       bool    `json:"ok"`
-	Output   string  `json:"output"`
+	// TaskID is the unique identifier for the task that produced this result.
+	TaskID string `json:"task_id"`
+	// Ok indicates whether the task was successful.
+	Ok bool `json:"ok"`
+	// Output is the output of the task, if successful.
+	Output string `json:"output"`
+	// Duration is the amount of time it took for the task to complete.
 	Duration float64 `json:"duration"`
 }
 
@@ -34,7 +48,9 @@ type customWriter struct {
 	mux     sync.Mutex
 }
 
-// Write is a custom writer for the command output.
+// Write writes the given bytes to the custom writer.
+//
+// Newline characters ('\n') are used to flush the pending buffer and append the current contents to the list of entries.
 func (c *customWriter) Write(b []byte) (int, error) {
 	var n int
 
@@ -53,6 +69,9 @@ func (c *customWriter) Write(b []byte) (int, error) {
 	return n, nil
 }
 
+// GetEntry returns the next entry from the custom writer, or nil if there are no more entries.
+//
+// If there are no more entries, the function returns an error.
 func (c *customWriter) GetEntry() ([]byte, error) {
 	c.mux.Lock()
 	defer c.mux.Unlock()
