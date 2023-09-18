0021ea203ce174602b94717186df85a9d5266b1c
Ben Lechlitner <otherland@gmail.com>
2023-09-18 01:27:00 -0400

    remove unused exercise package function

--- pkg/exercise/exercise.go
+++ pkg/exercise/exercise.go
@@ -2,14 +2,6 @@ package exercise
 
 import (
 	"fmt"
-	"path/filepath"
-	"regexp"
-	"strconv"
-	"strings"
-
-	"github.com/spf13/afero"
-
-	"github.com/asphaltbuffet/elf/pkg/utilities"
 )
 
 type Exercise struct {
@@ -22,33 +14,4 @@ type Exercise struct {
 
 func (e *Exercise) String() string {
 	return fmt.Sprintf("%d - %s", e.Day, e.Title)
-}
-
-var exerciseDirRegexp = regexp.MustCompile(`(?m)^(\d{2})-([a-zA-Z-,'"]+)$`)
-
-func ListingFromDir(fs afero.Fs, sourceDir string) ([]*Exercise, error) {
-	dirEntries, err := afero.ReadDir(fs, sourceDir)
-	if err != nil {
-		return nil, err
-	}
-
-	var out []*Exercise
-
-	for _, entry := range dirEntries {
-		if entry.IsDir() && exerciseDirRegexp.MatchString(entry.Name()) {
-			dir := entry.Name()
-
-			left, right, _ := strings.Cut(dir, "-")
-			dayInt, _ := strconv.Atoi(left) // error ignored because regex should have ensured this is ok
-			dayTitle := utilities.CamelToTitle(right)
-			out = append(out, &Exercise{
-				Day:   dayInt,
-				Title: dayTitle,
-				Dir:   right,
-				Path:  filepath.Join(sourceDir, dir),
-			})
-		}
-	}
-
-	return out, nil
 }
