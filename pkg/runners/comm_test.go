4f35c35cd66c514ba52fbf1868cbf5fa90fd4db7
Ben Lechlitner <otherland@gmail.com>
2023-09-18 14:22:28 -0400

    add unit tests for runners.GetEntry()

--- pkg/runners/comm_test.go
+++ pkg/runners/comm_test.go
@@ -1,6 +1,7 @@
 package runners
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -48,6 +49,43 @@ func Test_customWriter_Write(t *testing.T) {
 				assert.Equal(t, tt.want.entries, cw.entries)
 				assert.Equal(t, tt.want.pending, cw.pending)
 				assert.Equal(t, tt.want.n, got)
+			}
+		})
+	}
+}
+
+func Test_customWriter_GetEntry(t *testing.T) {
+	tests := []struct {
+		name      string
+		entries   [][]byte
+		want      []byte
+		assertion assert.ErrorAssertionFunc
+		errText   string
+	}{
+		{"no entries to get", [][]byte{}, nil, assert.Error, "no entries"},
+		{"single entry", [][]byte{[]byte("test data")}, []byte("test data"), assert.NoError, ""},
+		{
+			"multiple entries",
+			[][]byte{
+				[]byte("test data 1"),
+				[]byte("test data 2"),
+				[]byte("fake data 3"),
+			},
+			[]byte("test data 1"), assert.NoError, "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &customWriter{entries: tt.entries}
+
+			got, err := c.GetEntry()
+
+			tt.assertion(t, err)
+			if err != nil {
+				assert.ErrorContains(t, err, tt.errText)
+			} else {
+				assert.Equal(t, tt.want, got, fmt.Sprintf("expected %q, got %q", tt.want, got))
+				assert.NotContains(t, c.entries, got)
 			}
 		})
 	}
