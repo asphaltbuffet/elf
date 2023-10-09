5980321c32557e913ed2a3c8888426760045b701
Ben Lechlitner <otherland@gmail.com>
2023-10-08 23:07:40 -0400

    implement challenge interface for project euler

--- pkg/euler/euler.go
+++ pkg/euler/euler.go
@@ -1,24 +1,38 @@
 package euler
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/asphaltbuffet/elf/pkg/runners"
+)
 
 type Problem struct {
-	ID int
-
-	lang string
+	ID       int
+	Language string
 }
 
 func New(id int, lang string) *Problem {
 	return &Problem{
-		ID:   id,
-		lang: lang,
+		ID:       id,
+		Language: lang,
 	}
 }
 
 func (p *Problem) Solve() error {
+	fmt.Println("Solving", p)
 	return nil
 }
 
 func (p *Problem) String() string {
-	return fmt.Sprintf("Project Euler: %03d (%s)", p.ID, p.lang)
+	if p == nil {
+		return "Project Euler: NIL PROBLEM"
+	}
+
+	name, ok := runners.RunnerNames[p.Language]
+	if !ok {
+		name = "INVALID LANGUAGE"
+	}
+
+	return fmt.Sprintf("Project Euler: %03d (%s)", p.ID, name)
 }
