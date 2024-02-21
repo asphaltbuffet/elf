a6444fe1d2db7d18632a13b8c1cad3d37b740bc0
Ben Lechlitner <otherland@gmail.com>
2024-02-20 23:17:19 -0500

    Revert "add wrapper for path/filepath"
    This reverts commit 1e3d46520333280f48b6864a3755bc69247f7cff.

--- pkg/fileyourself/doc.go
+++
@@ -1,6 +0,0 @@
-// Package fileyourself implements utility routines for manipulating filename paths in a way
-// compatible with the target operating system-defined file paths.
-//
-// It can generally be used as a drop-in replacement for path/filepath with a notable
-// exception for the return values from filepath.Base() and filepath.Ext().
-package fileyourself
