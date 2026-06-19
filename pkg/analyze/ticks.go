ef2c49b895ddbac835b0c6ba5ef04c19417300b4
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-06-19 19:20:53 -0400

    feat(analyze): add relative log-tick marker labelled in multiples

--- pkg/analyze/ticks.go
+++ pkg/analyze/ticks.go
@@ -71,3 +71,45 @@ func dayTicker(minDay, maxDay float64) []plot.Tick {
 
 	return ticks
 }
+
+// RelativeLogTicks is a plot.Ticker for a log-scale axis of dimensionless
+// ratios: major ticks are labelled as multiples ("1×", "10×", ...) rather than
+// SI seconds. Use it for the relative-runtime box plot.
+type RelativeLogTicks struct{}
+
+var _ plot.Ticker = RelativeLogTicks{}
+
+// Ticks returns log-decade ticks over [minVal, maxVal], labelling each decade
+// as an integer multiple followed by "×".
+func (RelativeLogTicks) Ticks(minVal, maxVal float64) []plot.Tick {
+	if minVal <= 0 || maxVal <= 0 {
+		panic("Values must be greater than 0 for a log scale.")
+	}
+
+	const decade = 10
+
+	val := math.Pow10(int(math.Log10(minVal)))
+	maxLimit := math.Pow10(int(math.Ceil(math.Log10(maxVal))) + 1)
+
+	var ticks []plot.Tick
+
+	for val < maxLimit {
+		for i := 1; i < decade; i++ {
+			if i == 1 {
+				ticks = append(ticks, plot.Tick{
+					Value: val,
+					Label: fmt.Sprintf("%d×", int(val)),
+				})
+			}
+			ticks = append(ticks, plot.Tick{Value: val * float64(i)})
+		}
+		val *= decade
+	}
+
+	ticks = append(ticks, plot.Tick{
+		Value: val,
+		Label: fmt.Sprintf("%d×", int(val)),
+	})
+
+	return ticks
+}
